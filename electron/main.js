37747430017492ee88485fc34e8114c801940499
oura-hideyoshi <tomatommy.bs@gmail.com>
2023-08-09 22:08:32 +0900

    install prettier

--- electron/main.js
+++ electron/main.js
@@ -1,18 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, desktopCapturer } = require("electron");
+// In the preload script.
+const { ipcRenderer } = require("electron");
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-  app.whenReady().then(() => {
-    createWindow()
-  })
+  win.loadFile("index.html");
+};
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
